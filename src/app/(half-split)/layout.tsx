de0e839d7a89060ea6becad4335cb5942dff6426
Zakir Hussain <itsjakirhussain@gmail.com>
2024-02-15 23:45:18 +0530

    UI fix

--- src/app/(half-split)/layout.tsx
+++ src/app/(half-split)/layout.tsx
@@ -7,7 +7,7 @@ export default function HalfSplitLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="h-full w-full bg-base-300 grid md:grid-cols-2 grid-cols-1 md:px-10 lg:px-48 px-4 py-8 gap-4">
+    <div className="h-full w-full min-h-d-full bg-base-300 grid md:grid-cols-2 grid-cols-1 md:px-10 lg:px-48 px-4 py-8 gap-4">
       <div className="flex justify-center align-middle items-center">
         <div className="flex w-auto h-auto flex-col justify-center align-middle items-center select-none py-6 px-4">
           <PiAppWindowFill className="h-40 w-40 text-primary" />
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="min-h-d-full">
+      <body>
         <ThemeToggle />
         {children}
       </body>
